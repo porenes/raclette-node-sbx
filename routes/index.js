243b52d900bd9e74b45bf30ec0fa52a34b2feac9
patxol <pol@lemonbox.fr>
2020-04-08 23:37:10 +0200

    Improving homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+let welcome_messages = [
+  'In raclette we trust',
+  'There is no such thing as too much cheese',
+  'We love cheese 🧀'
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let mes_id = Math.round(Math.random()*(welcome_messages.length-1))
+  console.log(mes_id);
+  
+  res.render('index', 
+  { title: 'NodeJS Raclette Sandbox', welcome_message: welcome_messages[mes_id] });
 });
 
 module.exports = router;
